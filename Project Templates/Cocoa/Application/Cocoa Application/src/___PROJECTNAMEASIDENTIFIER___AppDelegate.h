5e83a6aee52e9daa15bff1cb1658f22b1c7d7466
Jason Kim <jason@512k.org>
2011-01-14 01:17:32 -0800

    Added project templates for cocoa applications.

---
+++ Project Templates/Cocoa/Application/Cocoa Application/src/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+@interface ___PROJECTNAMEASIDENTIFIER___AppDelegate :
+    NSObject <NSApplicationDelegate>
+{
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Project Templates/Cocoa/Application/Cocoa Application/src/___PROJECTNAMEASIDENTIFIER___AppDelegate.m
@@ -0,0 +1,17 @@
+#import "___PROJECTNAMEASIDENTIFIER___AppDelegate.h"
+
+@implementation ___PROJECTNAMEASIDENTIFIER___AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+    // ...
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    // ...
+}
+
+@end
