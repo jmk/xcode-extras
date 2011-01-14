67b1166c874a88787c88a49bc8fd42b3ca01611a
Jason Kim <jason@512k.org>
2011-01-14 00:41:28 -0800

    Added basic file templates.

---
+++ File Templates/Cocoa/NSObject subclass.pbfiletemplate/class.m
@@ -0,0 +1,21 @@
+«OPTIONALHEADERIMPORTLINE»
+@interface «FILEBASENAMEASIDENTIFIER» ()
+@end
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)init
+{
+    if (self = [super init]) {
+        // ...
+    }
+
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
---
+++ File Templates/Cocoa/NSView subclass.pbfiletemplate/class.m
@@ -0,0 +1,26 @@
+«OPTIONALHEADERIMPORTLINE»
+@interface «FILEBASENAMEASIDENTIFIER» ()
+@end
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)initWithFrame:(NSRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        // ...
+    }
+
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    // ...
+}
+
+@end
